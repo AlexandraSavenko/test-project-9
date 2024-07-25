b135b5f0705eb852c1092f113e2e10543027cf0e
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-07-25 12:11:01 +0300

    paggination doesn't work yet

--- src/main.js
+++ src/main.js
@@ -1,20 +1,6 @@
-const list = document.querySelector('.reviews-list');
-fetch('https://portfolio-js.b.goit.study/api/reviews')
-  .then(response => {
-    console.log(response);
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    const createListMarkup = createList(data);
-    list.insertAdjacentHTML('beforeend', createListMarkup);
-  })
-  .catch(error => {
-    console.log('Not found', error);
-  });
+import Swiper from 'swiper';
+import 'swiper/css';
+
 const createMarkup = ({ review, avatar_url, author }) => {
   return `<li class="rev-list-item">
         <img src="${avatar_url}" alt="${author}" class="rev-item-img">
@@ -22,7 +8,40 @@ const createMarkup = ({ review, avatar_url, author }) => {
         <p class="rev-item-text">${review}</p>
       </li>`;
 };
+
 const createList = arrayList => {
-  console.log(arrayList);
   return arrayList.map(createMarkup).join('');
 };
+
+const list = document.querySelector('.reviews-list');
+fetch('https://portfolio-js.b.goit.study/api/reviews')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    const createListMarkup = createList(data);
+    list.insertAdjacentHTML('beforeend', createListMarkup);
+
+    const swiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
+    });
+  })
+  .catch(error => {
+    console.log('Not found', error);
+  });
