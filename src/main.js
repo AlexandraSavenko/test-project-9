a51b1ebb1f7969d67f8664a7ee6c03fcf1bd7c56
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-07-26 21:44:54 +0300

    before ul

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+// import Swiper from 'swiper/bundle';
+// import 'swiper/swiper-bundle.min.css';
 
-const createMarkup = ({ review, avatar_url, author }) => {
+export const createMarkup = ({ review, avatar_url, author }) => {
   return `<li class="rev-list-item ">
         <img src="${avatar_url}" alt="${author}" class="rev-item-img">
         <h3 class="rev-item-title">${author}</h3>
@@ -9,11 +11,11 @@ const createMarkup = ({ review, avatar_url, author }) => {
       </li>`;
 };
 
-const createList = arrayList => {
+export const createList = arrayList => {
   return arrayList.map(createMarkup).join('');
 };
 
-const list = document.querySelector('.reviews-list');
+var list = document.querySelector('.reviews-list');
 fetch('https://portfolio-js.b.goit.study/api/reviews')
   .then(response => {
     if (!response.ok) {
@@ -24,27 +26,26 @@ fetch('https://portfolio-js.b.goit.study/api/reviews')
   .then(data => {
     const createListMarkup = createList(data);
     list.insertAdjacentHTML('beforeend', createListMarkup);
-
-    const swiper = new Swiper('.swiper-container', {
-      direction: 'horizontal',
-      loop: true,
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-      spaceBetween: 16,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-      },
-    });
   })
   .catch(error => {
     console.log('Not found', error);
   });
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 16,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
---
+++ src/swiper.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper('.mySwiper', {
+  direction: 'horizontal',
+  //   loop: true,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const createMarkup = ({ review, avatar_url, author }) => {
+  return `<div class="swiper-slide">
+        <img src="${avatar_url}" alt="${author}" class="rev-item-img">
+        <h3 class="rev-item-title">${author}</h3>
+        <p class="rev-item-text">${review}</p>
+      </div>`;
+};
+
+const createList = arrayList => {
+  return arrayList.map(createMarkup).join('');
+};
+const list = document.querySelector('.swiper-wrapper');
+// console.dir(list);
+fetch('https://portfolio-js.b.goit.study/api/reviews')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    const createListMarkup = createList(data);
+    // console.log(createListMarkup);
+    list.insertAdjacentHTML('beforeend', createListMarkup);
+  })
+  .catch(error => {
+    console.log('Not found', error);
+  });
